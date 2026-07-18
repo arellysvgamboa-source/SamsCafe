6d2e35d0fb889b7486b38249a2620385bcde5770
Arellys Villena <arellysvgamboa@gmail.com>
2026-07-18 09:42:18 -0500

    feat:  implementación de la clase canela

--- src/Complementos/Canela.java
+++ src/Complementos/Canela.java
@@ -1,4 +1,20 @@
 package Complementos;
+import Bebidas.*;
+import Decorator.*;
 
-public class Canela {
+public class Canela extends DecoradorCafe {
+
+    public Canela (Cafe cafe){
+        super(cafe);
+    }
+
+    @Override
+    public String descripcion() {
+        return cafe.descripcion() + " + canela ";
+    }
+
+    @Override
+    public double precio() {
+        return cafe.precio() + 1.8;
+    }
 }
