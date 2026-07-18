b0911cb68b4e79d09809b556aca91d82d81fb8d0
Arellys Villena <arellysvgamboa@gmail.com>
2026-07-18 09:33:16 -0500

    feat:  implementación de la clase latte

--- src/Bebidas/Latte.java
+++ src/Bebidas/Latte.java
@@ -1,4 +1,13 @@
 package Bebidas;
 
-public class Latte {
+public class Latte extends Cafe{
+    @Override
+    public String descripcion() {
+        return "Latte";
+    }
+
+    @Override
+    public double precio() {
+        return 8.5;
+    }
 }
