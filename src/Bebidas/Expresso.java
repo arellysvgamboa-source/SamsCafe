2a3a92457f8e1df9267f7ca7161df57bc2a41c94
Arellys Villena <arellysvgamboa@gmail.com>
2026-07-18 09:32:18 -0500

    feat:  implementación de la Expresso

--- src/Bebidas/Expresso.java
+++ src/Bebidas/Expresso.java
@@ -1,4 +1,14 @@
 package Bebidas;
 
-public class Expresso {
+public class Expresso extends Cafe {
+
+    @Override
+    public String descripcion() {
+        return "Cafe expreso";
+    }
+
+    @Override
+    public double precio() {
+        return 5.6;
+    }
 }
